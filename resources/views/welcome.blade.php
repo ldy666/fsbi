bc373cc9deaa20665da681f7bbf5c968cdf4bfd4
Melinda <dongyu.liu@melinda.clinic>
2018-10-31 16:25:39 +0800

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    fsbi
+                    fsbi1
                 </div>
 
                 <div class="links">
